56b547863a4f7fe152523dd18046fe7297782b33
Exodya21 <jaumegomiciai@gmail.com>
2023-03-09 00:32:09 +0100

    front 70%

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import './App.css';
+// import abcInfografia from "./asets/abc-unowns.png";
+import {unowns} from "./gif";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(query)
+  }
+
   return (
     <div className="container">
-      <h1>¡Conoce a los unowns!</h1>
+      <h1>¡Conoce a los Unowns!</h1>
+      <section>
+
+        <div className='gifs' >
+          {
+            // eslint-disable-next-line array-callback-return
+            unowns?.map( unown => (
+              <div key={unown.name} className='unown'>
+                <p>Letra {unown.char}</p>
+                <img src={unown.img} alt={unown.name}></img>
+              </div>
+            ))
+          }
+        </div>
+
+        <form onSubmit={onSubmit}>
+          <h2>Escribe cualquier cosa con la ayuda de los Unowns</h2>
+          <textarea
+            value={query}
+            onChange={e => setQuery(e.target.value) }
+          ></textarea>
+          <button type='submit'>Traducir</button>
+        </form>
+
+      </section>
+      <footer>
+        <p>gif of Unown by </p>
+        <a href="https://imgur.com/a/WXSvn#uD39I8E">imgur</a>
+      </footer>
     </div>
   );
 }
---
+++ src/gif.js
@@ -0,0 +1,173 @@
+import a from "./asets/unown-a.gif";
+import b from "./asets/unown-b.gif";
+import c from "./asets/unown-c.gif";
+import d from "./asets/unown-d.gif";
+import e from "./asets/unown-e.gif";
+import f from "./asets/unown-f.gif";
+import g from "./asets/unown-g.gif";
+import h from "./asets/unown-h.gif";
+import i from "./asets/unown-i.gif";
+import j from "./asets/unown-j.gif";
+import k from "./asets/unown-k.gif";
+import l from "./asets/unown-l.gif";
+import m from "./asets/unown-m.gif";
+import n from "./asets/unown-n.gif";
+import o from "./asets/unown-o.gif";
+import p from "./asets/unown-p.gif";
+import q from "./asets/unown-q.gif";
+import r from "./asets/unown-r.gif";
+import s from "./asets/unown-s.gif";
+import t from "./asets/unown-t.gif";
+import u from "./asets/unown-u.gif";
+import v from "./asets/unown-v.gif";
+import w from "./asets/unown-w.gif";
+import x from "./asets/unown-x.gif";
+import y from "./asets/unown-y.gif";
+import z from "./asets/unown-z.gif";
+import question from "./asets/unown-question.gif";
+import exclamation from "./asets/unown-exclamation.gif";
+
+
+
+export const unowns = [
+    {
+        char : "A",
+        name : "unown-a",
+        img : a
+    },
+    {
+        char : "B",
+        name : "unown-b",
+        img : b
+    },
+    {
+        char : "C",
+        name : "unown-c",
+        img : c
+    },
+    {
+        char : "D",
+        name : "unown-d",
+        img : d
+    },
+    {
+        char : "E",
+        name : "unown-e",
+        img : e
+    },
+    {
+        char : "F",
+        name : "unown-f",
+        img : f
+    },
+    {
+        char : "G",
+        name : "unown-g",
+        img : g
+    },
+    {
+        char : "H",
+        name : "unown-h",
+        img : h
+    },
+    {
+        char : "i",
+        name : "unown-i",
+        img : i
+    },
+    {
+        char : "J",
+        name : "unown-j",
+        img : j
+    },
+    {
+        char : "K",
+        name : "unown-k",
+        img : k
+    },
+    {
+        char : "L",
+        name : "unown-l",
+        img : l
+    },
+    {
+        char : "M",
+        name : "unown-m",
+        img : m
+    },
+    {
+        char : "N",
+        name : "unown-n",
+        img : n
+    },
+    {
+        char : "O",
+        name : "unown-o",
+        img : o
+    },
+    {
+        char : "P",
+        name : "unown-p",
+        img : p
+    },
+    {
+        char : "Q",
+        name : "unown-q",
+        img : q
+    },
+    {
+        char : "R",
+        name : "unown-r",
+        img : r
+    },
+    {
+        char : "S",
+        name : "unown-s",
+        img : s
+    },
+    {
+        char : "T",
+        name : "unown-t",
+        img : t
+    },
+    {
+        char : "U",
+        name : "unown-u",
+        img : u
+    },
+    {
+        char : "V",
+        name : "unown-v",
+        img : v
+    },
+    {
+        char : "W",
+        name : "unown-w",
+        img : w
+    },
+    {
+        char : "X",
+        name : "unown-x",
+        img : x
+    },
+    {
+        char : "Y",
+        name : "unown-y",
+        img : y
+    },
+    {
+        char : "Z",
+        name : "unown-z",
+        img : z
+    },
+    {
+        char : "?",
+        name : "unown-question",
+        img : question
+    },
+    {
+        char : "!",
+        name : "unown-exclamation",
+        img : exclamation
+    }
+]
